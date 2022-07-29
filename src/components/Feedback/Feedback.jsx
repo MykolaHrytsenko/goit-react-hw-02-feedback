b7fc53a12f504fbd5775f1f67a2d44f2858eae05
Mykola_Hrytsenko <94016020+MykolaHrytsenko@users.noreply.github.com>
2022-07-29 12:47:59 +0300

    save

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import css from './Feedback.module.css';
 
-export const Feedback = () => {
-  return (
-    <div className={css.container}>
-      <h1 className={css.title}>Please leave feedback</h1>
-      <button className={css.btn}></button>
-    </div>
-  );
-};
+export class Feedback extends React.Component {
+  render() {
+    return (
+      <div className={css.container}>
+        <h1 className={css.title}>Please leave feedback</h1>
+        <div className={css.btnWrapper}>
+          <button className={css.btn}>Good</button>
+          <button className={css.btn}>Neutral</button>
+          <button className={css.btn}>Bad</button>
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import css from './Statistics.module.css';
 
-export const Statistics = () => {
-  return (
-    <div className={css.container}>
-      <h2 className={css.title}>Statistics</h2>
-      <span className={css.item}>Good:</span>
-      <span className={css.item}>Neutral:</span>
-      <span className={css.item}>Bad:</span>
-    </div>
-  );
-};
+export class Statistics extends React.Component {
+  render() {
+    return (
+      <div className={css.container}>
+        <h2 className={css.title}>Statistics</h2>
+        <div className={css.wrapper}>
+          <span className={css.item}>Good:</span>
+          <span className={css.item}>Neutral:</span>
+          <span className={css.item}>Bad:</span>
+        </div>
+      </div>
+    );
+  }
+}
